3af176517345ee003404ff1073981603f09736d1
Kirill <uspenskiyke@mail.ru>
2022-07-27 15:26:25 +0400

    Домашняя работа: Коллекции и мапы.

--- src/main/java/com/flightofdream/HRService/HrServiceApplication.java
+++ src/main/java/com/flightofdream/HRService/HrServiceApplication.java
@@ -1,1 +1,9 @@
+package com.flightofdream.HRService;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrServiceApplication {
+
+}
