071f9da8d33235eb4cdc119f81576d0161637c3a
Kirill <uspenskiyke@mail.ru>
2022-07-27 15:06:02 +0400

    Домашняя работа: Коллекции и мапы.

--- src/main/java/com/flightofdream/HRService/EmployeeService.java
+++ src/main/java/com/flightofdream/HRService/EmployeeService.java
@@ -1,11 +1,1 @@
-package com.flightofdream.HRService;
 
-import java.util.HashMap;
-import java.util.List;
-
-public interface EmployeeService {
-    Employee addEmployee( String firstName, String lastName);
-    Employee removeEmployee( String firstName, String lastName);
-    Employee findEmployee(String firstName, String lastName);
-    HashMap<String,Employee> showEmployeeList();
-}
